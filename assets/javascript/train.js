67d8b4fd13f57324943308ace2376093478b02a8
John Nowinsky <armthepit@armthepit.com>
2016-09-18 15:40:27 -0500

    Add function to add new train to firebase

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,26 +1,44 @@
-$(document).ready(function(){
-
 	// Initialize Firebase
-	var config = {
-	apiKey: "AIzaSyBg9Qgnc-p3dNVNmUwechXZbZj1kI6NygY",
-	authDomain: "trainscheduler-34824.firebaseapp.com",
-	databaseURL: "https://trainscheduler-34824.firebaseio.com",
-	storageBucket: "trainscheduler-34824.appspot.com",
-	messagingSenderId: "948524545388"
-	};
-	firebase.initializeApp(config);
-
-	database = firebase.database();
+	var database = new Firebase("https://trainscheduler-34824.firebaseio.com");	
 
 	// Display Add Train Form when Add Train clicked
-	$("#displayAddTrain").on("click", function(){
+	$("#displayAddTrainBtn").on("click", function(){
 		$("#addTrain").removeClass("hide");	
 	});
 
+	// Add train to Firebase
 
+	$("#addTrainBtn").on("click", function(){
 
+		// Grabs user input
+		var train = $("#trainInput").val().trim();
+		var trainName = $("#trainNameInput").val().trim();
+		var destination = $("#destinationInput").val().trim();
+		var firstDeparture = $("#firstDepartureInput").val().trim();
+		var frequency = $("#frequencyInput").val().trim();
 
+		// Creates local "temporary" object for holding employee data
+		var newTrain = {
+			train:  train,
+			trainName: trainName,
+			destination: destination,
+			firstDeparture: firstDeparture,
+			frequency: frequency
+		}
 
+		// Upload train data to Firebase
+		database.push(newTrain);
 
+		// Alert
+		alert("Train successfully added");
 
-});
+		// Clears all of the text-boxes
+		$("#trainInput").val().trim("");
+		$("#trainNameInput").val().trim("");
+		$("#destinationInput").val().trim("");
+		$("#firstDepartureInput").val().trim("");
+		$("#frequencyInput").val().trim("");
+
+		// Determine when the next train arrives.
+		return false;
+	});
