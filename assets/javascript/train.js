5903590a7d6782e4e57bd21361cd036f55963eba
John Nowinsky <armthepit@armthepit.com>
2016-09-18 14:17:04 -0500

    Add firebase to html/js

---
+++ assets/javascript/train.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	// Initialize Firebase
+	var config = {
+	apiKey: "AIzaSyBg9Qgnc-p3dNVNmUwechXZbZj1kI6NygY",
+	authDomain: "trainscheduler-34824.firebaseapp.com",
+	databaseURL: "https://trainscheduler-34824.firebaseio.com",
+	storageBucket: "trainscheduler-34824.appspot.com",
+	messagingSenderId: "948524545388"
+	};
+	firebase.initializeApp(config);
+
+	database = firebase.database();
+
+
+
+
+
+
+
+
+
+});
